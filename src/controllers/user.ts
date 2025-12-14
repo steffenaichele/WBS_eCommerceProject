27b6fe8fc178ac5318b825d5e5cb34240021495b
Steffen Aichele <steffen.aichele@icloud.com>
2025-12-14 21:57:33 +0100

    added user controller

---
+++ src/controllers/user.ts
@@ -0,0 +1,47 @@
+import { User } from "#models";
+import type { UserDTO, UserInputDTO } from "#schemas";
+import type { RequestHandler } from "express";
+
+// GET /users
+const getAllUsers: RequestHandler<{}, UserDTO[]> = async (req, res) => {
+	const users = await User.find();
+	res.json(users);
+};
+
+// Post /users
+const createUser: RequestHandler<{}, UserDTO, UserInputDTO> = async (req, res) => {
+	const newUser = await User.create<UserInputDTO>(req.body);
+	res.status(201).json(newUser);
+};
+
+// GET /users/:id
+const getUserById: RequestHandler<{ id: string }, UserDTO> = async (req, res) => {
+	const { id } = req.params;
+	const user = await User.findById(id);
+	if (!user) throw new Error("User not found", { cause: 404 });
+	res.json(user);
+};
+
+// PUT /users/:id
+const updateUser: RequestHandler<{ id: string }, UserDTO, UserInputDTO > = async (req, res) => {
+	const { id } = req.params;
+	const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
+	if (!updatedUser) throw new Error("User not found", { cause: 404 });
+	res.json(updatedUser);
+};
+
+// DELETE /users/:id
+const deleteUser: RequestHandler<{ id: string }, { message: string }, UserInputDTO> = async (req, res) => {
+	const { id } = req.params;
+	const foundUser = await User.findByIdAndDelete(id);
+	if (!foundUser) throw new Error("User not found", { cause: 404 });
+	res.json({ message: "User deleted" });
+}
+
+export {
+	getAllUsers,
+	createUser,
+	getUserById,
+	updateUser,
+	deleteUser,
+};
